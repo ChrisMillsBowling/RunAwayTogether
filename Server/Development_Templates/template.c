8fcaeb964ab2227b09b2cf0b014b4f399f64afdc
Chris Mills-Bowling <chrismillsbowlingpro@gmail.com>
2021-07-27 18:20:51 +0000

    Added the template files

---
+++ Server/Development_Templates/template.c
@@ -0,0 +1,27 @@
+/**
+ * @file template.c
+ * @author Chris Mills-Bowling (chrismillsbowlingpro@gmail.com)
+ * @brief This is the template file for C BARR standard for Embedded C
+ * @version 0.1
+ * @date 2021-07-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#include "template.h"
+
+//Data Type Definitions
+
+//Constant Definitions
+
+//Macro Definitions
+
+//Static data declarations
+
+//private static function prototypes
+
+//public function bodies
+
+//private static function bodies
---
+++ Server/Development_Templates/template.h
@@ -0,0 +1,25 @@
+/**
+ * @file template.h
+ * @author Chris Mills-Bowling (chrismillsbowlingpro@gmail.com)
+ * @brief This is the header template for C BARR coding standard.
+ * @version 0.1
+ * @date 2021-07-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef TEMPLATE
+#define TEMPLATE   
+
+//Include Section
+
+//Macro section. #define
+
+//Data type section. typedef
+
+//Public function section. Prototypes
+
+
+#endif
+
